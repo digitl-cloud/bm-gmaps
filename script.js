24cb90fe6c00ad77691d2c66066cfbfdfb22215a
Marco Schierhorn <marcoschierhorn@Marcos-MacBook.local>
2022-08-30 17:21:39 +0200

    Initial draft of the Google Maps API PoC for B&M

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+ document.addEventListener('DOMContentLoaded', function () {
   if (document.querySelectorAll('#map').length > 0)
   {
     if (document.querySelector('html').lang)
@@ -8,19 +8,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language=' + lang;
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language=' + lang + '&key=AIzaSyDvHrH49ggg4zB2tpRsu8I6ygD989Bx7go';
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
 
 var map;
 
+// API Key: AIzaSyDvHrH49ggg4zB2tpRsu8I6ygD989Bx7go
+
 function initMap()
 {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: -33.4569400, lng: -70.6482700},
     zoom: 8
   });
+
+  // fetch-Aufruf mit Pfad zur XML-Datei
+  fetch ('KZK126.xml')
+    .then (function (response) {
+      // Antwort kommt als Text-String
+      return response.text();
+    })
+    .then (function (data) {
+      //console.log (data);       // schnell mal in der Konsole checken
+
+      var x2js = new X2JS();
+
+      var jsonObj = x2js.xml_str2json(data);
+
+      var airTravel = jsonObj.root.airTravel;
+      var country = jsonObj.root.country;
+      var itinerary = jsonObj.root.itinerary;
+      var location = jsonObj.root.location;
+      var organizer = jsonObj.root.organizer;
+
+      plotMarkers(itinerary.day);
+
+    }).catch (function (error) {
+       console.log ("Fehler: bei Auslesen der XML-Datei " + error);
+    });
 
   fetch('markers.json')
     .then(function(response){return response.json()})
@@ -30,23 +57,43 @@ function initMap()
 var markers;
 var bounds;
 
-function plotMarkers(m)
+function plotMarkers(days)
 {
   markers = [];
   bounds = new google.maps.LatLngBounds();
 
-  m.forEach(function (marker) {
-    var position = new google.maps.LatLng(marker.lat, marker.lng);
+  console.log('plotMarkers');
+  console.log(days);
 
-    markers.push(
-      new google.maps.Marker({
-        position: position,
-        map: map,
-        animation: google.maps.Animation.DROP
-      })
-    );
+  days.forEach(function (day) {
+    console.log(day);
 
-    bounds.extend(position);
+    if (typeof day.destination_ref.destination != "undefined") {
+      console.log(typeof day.destination_ref.destination);
+      var position = new google.maps.LatLng(day.destination_ref.destination.latitude, day.destination_ref.destination.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        content: day.name,
+      });
+
+      var marker = new google.maps.Marker({
+          position: position,
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: day.name
+        });
+
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
+
+      markers.push(marker)
+      bounds.extend(position);
+    }
   });
 
   map.fitBounds(bounds);
