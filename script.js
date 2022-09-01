03c2966a3303296f70111c7e4e4505246017331b
Marco Schierhorn <marcoschierhorn@Marcos-MacBook.local>
2022-09-01 11:51:39 +0200

    Added markers for days with arrays

--- script.js
+++ script.js
@@ -14,19 +14,19 @@
 });
 
 
-var map;
-var service;
-var localContextMap;
-var placesService;
+ var map;
+ var service;
+ var localContextMap;
+ var placesService;
 
-function initMap()
-{
+ function initMap()
+ {
 
   localContextMap = new google.maps.localContext.LocalContextMapView({
     element: document.getElementById("map"),
     placeTypePreferences: [
-      { type: "restaurant" },
-      { type: "tourist_attraction" },
+    { type: "restaurant" },
+    { type: "tourist_attraction" },
     ],
     maxPlaceCount: 0
   });
@@ -41,30 +41,26 @@ function initMap()
 
   // fetch-Aufruf mit Pfad zur XML-Datei
   fetch ('KZK126.xml')
-    .then (function (response) {
-      return response.text();
-    })
-    .then (function (data) {
+  .then (function (response) {
+    return response.text();
+  })
+  .then (function (data) {
 
-      var x2js = new X2JS();
+    var x2js = new X2JS();
 
-      var jsonObj = x2js.xml_str2json(data);
+    var jsonObj = x2js.xml_str2json(data);
 
-      var airTravel = jsonObj.root.airTravel;
-      var country = jsonObj.root.country;
-      var itinerary = jsonObj.root.itinerary;
-      var location = jsonObj.root.location;
-      var organizer = jsonObj.root.organizer;
+    var airTravel = jsonObj.root.airTravel;
+    var country = jsonObj.root.country;
+    var itinerary = jsonObj.root.itinerary;
+    var location = jsonObj.root.location;
+    var organizer = jsonObj.root.organizer;
 
-      plotMarkers(itinerary.day);
+    iterateOverDays(itinerary.day);
 
-    }).catch (function (error) {
-       console.log ("Fehler: bei Auslesen der XML-Datei " + error);
-    });
-
-  fetch('markers.json')
-    .then(function(response){return response.json()})
-    .then(plotMarkers);
+  }).catch (function (error) {
+   console.log ("Fehler: bei Auslesen der XML-Datei " + error);
+ });
 }
 
 var markers;
@@ -72,60 +68,69 @@ var bounds;
 var coords;
 var counter = 0;
 
-function plotMarkers(days)
+function createMarkers(dayName, destination) {
+ var position = new google.maps.LatLng(destination.latitude, destination.longitude);
+
+
+ var infoWindowContent = '<div class="container"><div class="accommodation-overlay h-100 w-100">'+dayName+'</div><div class="class="description"">'+destination.name+'</div></div>';
+
+ var infowindow = new google.maps.InfoWindow({
+  content: infoWindowContent,
+});
+
+ var marker = new google.maps.Marker({
+  position: position,
+  map: map,
+  animation: google.maps.Animation.DROP,
+  title: dayName
+});
+
+ marker.addListener("click", () => {
+  infowindow.open({
+    anchor: marker,
+    map,
+    shouldFocus: true,
+  });
+  localContextMap.maxPlaceCount = 12;
+  localContextMap.search();
+});
+
+ coords.push(position);
+ markers.push(marker)
+ bounds.extend(position);
+}
+
+function iterateOverDays(days)
 {
   markers = [];
   coords = [];
   bounds = new google.maps.LatLngBounds();
 
-  console.log('plotMarkers');
   console.log(days);
 
   days.forEach(function (day) {
     console.log(day);
     counter++;
 
-    if (typeof day.destination_ref.destination != "undefined" && day.destination_ref.destination.latitude && day.destination_ref.destination.longitude) {
-      console.log(typeof day.destination_ref.destination);
-      var position = new google.maps.LatLng(day.destination_ref.destination.latitude, day.destination_ref.destination.longitude);
-
-
-      var infoWindowContent = "<h3>"+day.name+"</h3><strong>"+day.destination_ref.destination.name+"</strong>";
-
-      var infowindow = new google.maps.InfoWindow({
-        content: infoWindowContent,
-      });
-
-      var marker = new google.maps.Marker({
-          position: position,
-          map: map,
-          animation: google.maps.Animation.DROP,
-          title: day.name
+    if (typeof day.destination_ref.destination != "undefined" && ((day.destination_ref.destination.latitude && day.destination_ref.destination.longitude) || Array.isArray(day.destination_ref.destination)) )  {
+      if (Array.isArray(day.destination_ref.destination)) {
+        day.destination_ref.destination.forEach(function (destinationDay, index) {
+          createMarkers(day.name, destinationDay);
         });
+      } else {
+        createMarkers(day.name, day.destination_ref.destination);
+      }
 
-      marker.addListener("click", () => {
-        infowindow.open({
-          anchor: marker,
-          map,
-          shouldFocus: true,
-        });
-        localContextMap.maxPlaceCount = 12;
-        localContextMap.search();
-      });
-
-      coords.push(position);
-      markers.push(marker)
-      bounds.extend(position);
     }
   });
 
   var flightPath = new google.maps.Polyline({
-          path: coords,
-          geodesic: true,
-          strokeColor: '#00FFFF',
-          strokeOpacity: 1.0,
-          strokeWeight: 5
-        });
+    path: coords,
+    geodesic: true,
+    strokeColor: '#00FFFF',
+    strokeOpacity: 1.0,
+    strokeWeight: 5
+  });
   
   flightPath.setMap(map);
   map.fitBounds(bounds);
